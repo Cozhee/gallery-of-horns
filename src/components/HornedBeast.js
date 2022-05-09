649670990e5ac01e452a3759511afabf7daed9fb
Cody Davis <codydavis@Codys-MacBook-Pro.local>
2022-05-09 15:04:14 -0700

    added one beast

---
+++ src/components/HornedBeast.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+    render() {
+        return (
+            <>
+                <h2>{this.props.title}</h2>
+                <img style={{ width: 200 }} title="horned" alt="text for it" src={this.props.imageUrl}></img>
+                <p>{this.props.description}</p>
+            </>
+        )
+    }
+}
+
+export default HornedBeast
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HornedBeast from './HornedBeast';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <>
+                <HornedBeast title="donkey" imageUrl="https://d.newsweek.com/en/full/2010784/donkey.jpg?w=1600&h=1600&q=88&f=fabb297756d66f322afe2b5fa86205ef" description="ass" />
+            </>
+        )
+    }
+}
+
+export default Main
